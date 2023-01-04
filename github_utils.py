be391747cb62b7662af33f6feda6dda0cc9910c8
jason5701 <jasoon5701@gmail.com>
2023-01-04 10:22:18 +0900

    git issue test

--- github_utils.py
+++ github_utils.py
@@ -1,8 +1,8 @@
 from github import Github
 
 def get_github_repo(access_token, repo_name):
-  git = Github(access_token)
-  return git.get_user().get_repo(repo_name)
+  g = Github(access_token)
+  return g.get_user().get_repo(repo_name)
 
 def upload_github_issue(repo, title, body):
   repo.create_issue(title=title,body=body)
---
+++ issue.py
@@ -0,0 +1,14 @@
+import os
+from github import Github
+
+if __name__=='__main__':
+  access_token = os.environ['MY_GITHUB_TOKEN']
+  title = '깃 이슈 테스트 제목'
+  body = '깃 이슈 테스트 내용입니다'
+
+  repo_name = "dayStocks"
+
+  g = Github(access_token)
+  
+  repo = g.get_user().get_repo(repo_name)
+  repo.create_issue(title=title, body=body)
