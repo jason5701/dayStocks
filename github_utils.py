510e0bf544ab8678deaa7a79544ae6c0524b49e0
jason5701 <jasoon5701@gmail.com>
2023-01-16 09:17:59 +0900

    update title

--- github_utils.py
+++ github_utils.py
@@ -5,4 +5,4 @@ def get_github_repo(access_token, repo_name):
   return g.get_user().get_repo(repo_name)
 
 def upload_github_issue(repo, title, body):
-  repo.create_issue(title=title+' '+len(body)+'건',body=body)
+  repo.create_issue(title=title+' '+str(len(body))+'건',body=body)
