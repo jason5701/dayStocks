45f36a057fc809401e39126fd3720ef7585c425c
jason5701 <jasoon5701@gmail.com>
2023-01-18 10:34:12 +0900

    update telegram

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from crawler import extract_stock_report
 from datetime import datetime
 from pytz import timezone
-# from send import telegram
+from send import telegram
 
 if __name__ == '__main__':
   # access_token = os.environ['MY_GITHUB_TOKEN']
@@ -20,8 +20,6 @@ if __name__ == '__main__':
   # upload_github_issue(repo, title, upload_contents)
 
   if(upload_contents != '없음'):
-    print(len(upload_contents))
-    print(upload_contents)
-    # telegram(upload_contents)
+    telegram(upload_contents)
 
   print('Upload done')
