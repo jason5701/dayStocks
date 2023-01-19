c465859b072453ff5f1106208a45ce380e40d4af
Junghun KIM <76275965+jason5701@users.noreply.github.com>
2023-01-19 11:23:16 +0900

    Update send.py

--- send.py
+++ send.py
@@ -1,10 +1,8 @@
 import telebot
 import os
 
-TELEGRAM_TOKEN='5636747753:AAH_JLSq1hhLB5kIHQfoVbpVxVeN6menFUU'
-# TELEGRAM_TOKEN=os.environ['TELEGRAM_TOKEN']
-CHAT_ID='5697250246'
-# CHAT_ID=os.environ['CHAT_ID']
+TELEGRAM_TOKEN=os.environ['TELEGRAM_TOKEN']
+CHAT_ID=os.environ['CHAT_ID']
 bot = telebot.TeleBot(token=TELEGRAM_TOKEN)
 
 def telegram(body):
